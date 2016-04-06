93c26f56e71713ab524737044a744c72e1771005
Daniel Hahler <git@thequod.de>
2016-04-06 16:09:39 +0200

    Make pattern for removal of existing lines more specific
    Explicitly match the file name in the second tab-delimited column.
    
    Also, add a "|| true" to handle the case where no lines would be
    selected by grep (e.g. after `touch tags` and only updating a single
    file).

--- plat/unix/update_tags.sh
+++ plat/unix/update_tags.sh
@@ -73,8 +73,10 @@ INDEX_WHOLE_PROJECT=1
 if [ -f "$TAGS_FILE" ]; then
     if [ "$UPDATED_SOURCE" != "" ]; then
         echo "Removing references to: $UPDATED_SOURCE"
-        echo "grep -v \"$UPDATED_SOURCE\" \"$TAGS_FILE\" > \"$TAGS_FILE.temp\""
-        grep -v "$UPDATED_SOURCE" "$TAGS_FILE" > "$TAGS_FILE.temp"
+        tab="	"
+        cmd="grep -Ev '^[^$tab]+$tab$UPDATED_SOURCE$tab' '$TAGS_FILE' > '$TAGS_FILE.temp'"
+        echo "$cmd"
+        eval "$cmd" || true
         INDEX_WHOLE_PROJECT=0
     fi
 fi
