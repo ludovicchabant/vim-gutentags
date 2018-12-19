0ce29a9cd32af0ed9eea17f27da6f4d35a709a5d
Chayoung You <yousbe@gmail.com>
2018-12-19 20:49:22 +0900

    Add `g:gutentags_cscope_build_inverted_index` option

--- plat/unix/update_scopedb.sh
+++ plat/unix/update_scopedb.sh
@@ -8,6 +8,7 @@ CSCOPE_ARGS=
 DB_FILE=cscope.out
 PROJECT_ROOT=
 FILE_LIST_CMD=
+BUILD_INVERTED_INDEX=0
 
 ShowUsage() {
     echo "Usage:"
@@ -17,11 +18,12 @@ ShowUsage() {
     echo "    -f [file=cscope.out]: The path to the ctags file to update"
     echo "    -p [dir=]:            The path to the project root"
     echo "    -L [cmd=]:            The file list command to run"
+    echo "    -I:                   Builds an inverted index"
     echo ""
 }
 
 
-while getopts "h?e:f:p:L:" opt; do
+while getopts "h?e:f:p:L:I" opt; do
     case $opt in
         h|\?)
             ShowUsage
@@ -39,6 +41,9 @@ while getopts "h?e:f:p:L:" opt; do
         L)
             FILE_LIST_CMD=$OPTARG
             ;;
+        I)
+            BUILD_INVERTED_INDEX=1
+            ;;
     esac
 done
 
@@ -53,7 +58,14 @@ echo "Locking cscope DB file..."
 echo $$ > "$DB_FILE.lock"
 
 # Remove lock and temp file if script is stopped unexpectedly.
-trap 'rm -f "$DB_FILE.lock" "$DB_FILE.files" "$DB_FILE.temp"' INT QUIT TERM EXIT
+CleanUp() {
+    rm -f "$DB_FILE.lock" "$DB_FILE.files" "$DB_FILE.temp"
+    if [ "$BUILD_INVERTED_INDEX" -eq 1 ]; then
+        rm -f "$DB_FILE.temp.in" "$DB_FILE.temp.po"
+    fi
+}
+
+trap CleanUp INT QUIT TERM EXIT
 
 PREVIOUS_DIR=$(pwd)
 if [ -d "$PROJECT_ROOT" ]; then
@@ -74,6 +86,10 @@ else
 fi
 CSCOPE_ARGS="${CSCOPE_ARGS} -i ${DB_FILE}.files"
 
+if [ "$BUILD_INVERTED_INDEX" -eq 1 ]; then
+    CSCOPE_ARGS="$CSCOPE_ARGS -q"
+fi
+
 echo "Running cscope"
 echo "$CSCOPE_EXE $CSCOPE_ARGS -b -k -f \"$DB_FILE.temp\""
 "$CSCOPE_EXE" $CSCOPE_ARGS -v -b -k -f "$DB_FILE.temp"
@@ -83,6 +99,12 @@ if [ -d "$PROJECT_ROOT" ]; then
 fi
 
 echo "Replacing cscope DB file"
+if [ "$BUILD_INVERTED_INDEX" -eq 1 ]; then
+    echo "mv -f \"$DB_FILE.temp.in\" \"$DB_FILE.in\""
+    mv -f "$DB_FILE.temp.in" "$DB_FILE.in"
+    echo "mv -f \"$DB_FILE.temp.po\" \"$DB_FILE.po\""
+    mv -f "$DB_FILE.temp.po" "$DB_FILE.po"
+fi
 echo "mv -f \"$DB_FILE.temp\" \"$DB_FILE\""
 mv -f "$DB_FILE.temp" "$DB_FILE"
 
--- plat/win32/update_scopedb.cmd
+++ plat/win32/update_scopedb.cmd
@@ -10,6 +10,7 @@ set CSCOPE_ARGS=
 set DB_FILE=cscope.out
 set FILE_LIST_CMD=
 set LOG_FILE=
+set BUILD_INVERTED_INDEX=0
 
 :ParseArgs
 if [%1]==[] goto :DoneParseArgs
@@ -36,6 +37,10 @@ if [%1]==[-L] (
 if [%1]==[-l] (
     set LOG_FILE=%~2
     shift
+    goto :LoopParseArgs
+)
+if [%1]==[-I] (
+    set BUILD_INVERTED_INDEX=1
     goto :LoopParseArgs
 )
 echo Invalid Argument: %1
@@ -70,6 +75,9 @@ if NOT ["%FILE_LIST_CMD%"]==[""] (
 ) ELSE (
     set CSCOPE_ARGS=%CSCOPE_ARGS% -R
 )
+if ["%BUILD_INVERTED_INDEX%"]==["1"] (
+    set CSCOPE_ARGS=%CSCOPE_ARGS% -q
+)
 "%CSCOPE_EXE%" %CSCOPE_ARGS% -b -k -f "%DB_FILE%"
 if ERRORLEVEL 1 (
     echo ERROR: Cscope executable returned non-zero code. >> %LOG_FILE%
@@ -99,5 +107,6 @@ echo    -f [file=cscope.out]:        The path to the database file to create
 echo    -p [dir=]:                   The path to the project root
 echo    -L [cmd=]:                   The file list command to run
 echo    -l [log=]:                   The log file to output to
+echo    -I:                          Builds an inverted index
 echo.
 
